2428ae417ba4678f3afd5c95881c71002c79630d
Hudson Menezes <hudson.bm23@gmail.com>
2023-01-27 11:53:35 -0300

    conexão com mongodb completa

--- src/server.js
+++ src/server.js
@@ -1,33 +1,24 @@
 import express from "express";
-import { MongoClient } from "mongodb";
+import { db, connectToDb } from "./db.js";
 
 const app = express();
 app.use(express.json());
 
 app.get("/api/articles/:name", async (req, res) => {
   const { name } = req.params;
-  const client = new MongoClient("mongodb://127.0.0.1:27017");
-  await client.connect();
-
-  const db = client.db("react-blog-db");
-
   const article = await db.collection("articles").findOne({ name });
 
   if (article) {
     res.json(article);
   } else {
-    res.sendStatus(404);
+    res.status(404);
   }
-  res.json(article);
+  // res.json(article);
 });
 
 app.put("/api/articles/:name/upvote", async (req, res) => {
   const { name } = req.params;
 
-  const client = new MongoClient("mongodb://127.0.0.1:27017");
-  await client.connect();
-
-  const db = client.db("react-blog-db");
   await db.collection("articles").updateOne(
     { name },
     {
@@ -37,27 +28,34 @@ app.put("/api/articles/:name/upvote", async (req, res) => {
   const article = await db.collection("articles").findOne({ name });
 
   if (article) {
-    article.upvotes += 1;
     res.send(`O artigo ${name} agora tem ${article.upvotes} votos!`);
   } else {
     res.send("Este artigo não existe.");
   }
 });
 
-app.post("/api/articles/:name/comments", (req, res) => {
+app.post("/api/articles/:name/comments", async (req, res) => {
   const { name } = req.params;
   const { postedBy, text } = req.body;
 
-  const article = articlesInfo.find((a) => a.name === name);
+  await db.collection("articles").updateOne(
+    { name },
+    {
+      $push: { comments: { postedBy, text } },
+    }
+  );
+  const article = await db.collection("articles").findOne({ name });
 
   if (article) {
-    article.comments.push({ postedBy, text });
     res.send(article.comments);
   } else {
     res.send("Este artigo não existe");
   }
 });
 
-app.listen(8000, () => {
-  console.log("Servidor está rodando na porta 8000!");
+connectToDb(() => {
+  console.log("Conectado ao banco de dados com sucesso!");
+  app.listen(8000, () => {
+    console.log("Servidor está rodando na porta 8000!");
+  });
 });
